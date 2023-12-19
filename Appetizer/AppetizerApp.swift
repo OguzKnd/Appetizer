1fc23bbf5e6292d500c3a76f659d50245a840778
Oguz Kanda <exporler95@gmail.com>
2023-12-19 15:40:36 +0300

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Oğuz Kanda on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
